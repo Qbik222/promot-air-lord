bf9de3da0e96c09452c5f3b2d3efbbdedfa75f62
yurii <ignatovyw@gmail.com>
2025-03-04 12:48:46 +0200

    upd promo template

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    root: true,
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+    },
+    extends: ['eslint:recommended'],
+    parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+    },
+    rules: {
+        // Scope-related rules
+        'no-undef': 'error',
+        'no-unused-vars': ['warn', { 
+            "vars": "all",
+            "args": "after-used",
+            "ignoreRestSiblings": false
+        }],
+        'no-use-before-define': ['error', { 
+            "functions": false, 
+            "classes": true, 
+            "variables": true 
+        }],
+        'block-scoped-var': 'error',
+        'no-shadow': 'error',
+        'no-var': 'error',
+        'prefer-const': 'error',
+        'no-undefined': 'error',
+        'init-declarations': ['error', 'always'],
+    }
+};
